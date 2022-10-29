0fcccbd31a55ee7e4f05cf55f6c3a7724248cf25
Michael Paglione <pagman777@gmail.com>
2022-10-29 17:17:05 -0400

    rename resume file, fix link in Navbar component, both due to typos

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -23,7 +23,7 @@ export default function Navbar() {
 
           <a href="#about" className="nav-link">About</a>
           <a className="nav-link"
-            href="/paglione_resume_10_22_29.pdf"
+            href="/paglione_resume_10_29_22.pdf"
             download
             role="button">Resume</a>
           <a href="#portfolio" className="nav-link">Portfolio</a>
